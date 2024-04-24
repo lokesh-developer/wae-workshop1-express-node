e36a93603c35e9fac89dc7c4dbf01070588ccc3c
Lokesh Kabra <kabral1999@gmail.com>
2024-04-24 22:38:53 +0530

    ..

--- app.js
+++ app.js
@@ -16,24 +16,39 @@ const taskSchema = new Schema({
 const Task = mongoose.model('Task', taskSchema);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) =>
+{
+    res.json({message:'Hello World!'})
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-app.post('/add', async (req, res) => {
-    const { name } = req.query;
+app.post('/add', jsonParser, async (req, res) =>
+{
+    const { name } = req.body;
     const newTask = new Task({
         name: name
     });
 
     newTask.save()
-        .then((post) => res.status(200).send('New blog post saved:', post))
-        .catch((error) => console.status(500).send('Error saving blog post:', error))
+        .then((post) => res.status(200).send('New task saved:', post))
+        .catch((error) => res.status(500).send('Error saving blog post:', error))
 })
+
+app.get("/get", async (req, res) =>
+{
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving blog posts' });
+    }
+})
+
+
+app.listen(port, () =>
+{
+    console.log(`Example app listening on port ${port}`)
+})
+
 
 
 
